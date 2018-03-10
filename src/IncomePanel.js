07e42259245967b868c9f94ea81ec32c051b7d64
Joshua C. Burt <joshburt@shapeandshare.com>
2018-03-10 09:16:21 -0700

    lots of updates for workers panel

--- src/IncomePanel.js
+++ src/IncomePanel.js
@@ -2,31 +2,65 @@ import React, { Component } from 'react';
 import './IncomePanel.css';
 
 class IncomePanel extends Component {
+    addWorker(textual_source_name){
+        this.props.model.updateUserIncomeSourceAllotment(textual_source_name, 1);
+    }
+
+    removeWorker(textual_source_name){
+        this.props.model.updateUserIncomeSourceAllotment(textual_source_name, -1);
+    }
+
+    buildAddButton(textual_source_name, label, key_suffix) {
+        let element_key = '';
+        element_key += key_suffix + '_' + label;
+        return (<button key={element_key} onClick={(e) => this.addWorker(textual_source_name)}>{label}</button>);
+    }
+
+    buildRemoveButton(textual_source_name, label, key_suffix) {
+        let element_key = '';
+        element_key += key_suffix + '_' + label;
+        return (<button key={element_key} onClick={(e) => this.removeWorker(textual_source_name)}>{label}</button>);
+    }
+
+    buildLabel(text, key_suffix){
+        let element_key = '';
+        element_key += key_suffix + '_' + text;
+        return(<label key={element_key}>{text}</label>);
+    }
+
+    buildBreak(key_suffix){
+        return(<br key={key_suffix}></br>);
+    }
+
+
+
     buildIncome() {
-        let incomeString = '';
-        if (this.props.model.income == null) {
-            incomeString = '|\n';
-        }
-        else{
+        let panelElements = [];
+        if (this.props.model.income != null) {
             for (var key in this.props.model.income){
+                let incomeString = '';
                 var income_obj = this.props.model.income[key]
                 var amount = income_obj['amount']
                 var description = income_obj['description']
-                incomeString += "| " + key + " (" + amount + ")"
+                incomeString += key + " (" + amount + ")"
                 if (description != null) {
                     incomeString += " (" + description + ")";
                 }
-                incomeString += "\n";
+                panelElements.push(this.buildLabel('| ', key));
+                panelElements.push(this.buildAddButton(key, '+', key));
+                panelElements.push(this.buildLabel(incomeString, key));
+                panelElements.push(this.buildRemoveButton(key, '-', key));
+                panelElements.push(this.buildBreak(key));
             }
         }
-        return incomeString;
+        return panelElements;
     }
 
     render() {
         return (
-            <div className="IncomePanel">
-                <div>+-- stores ----------------</div>
-                <div>{this.buildIncome()}</div>
+            <div key="IncomePanel" className="IncomePanel">
+                <div>+-- workers --------------</div>
+                {this.buildIncome()}
                 <div>+--------------------------</div>
             </div>
         );

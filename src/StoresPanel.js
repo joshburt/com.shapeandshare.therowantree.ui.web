5977e19bc07ae24086112bbaea4fb19793e20260
Joshua C. Burt <joshburt@shapeandshare.com>
2018-04-07 16:08:49 -0600

    converted over to props provided menu lib

--- src/StoresPanel.js
+++ src/StoresPanel.js
@@ -2,32 +2,40 @@ import React, { Component } from 'react';
 import './StoresPanel.css';
 
 class StoresPanel extends Component {
-    buildStores() {
-        let storesString = '';
+    buildStoresPanel() {
+        let panelElements = [];
+
+        panelElements.push(this.props.menu.buildLabel(this.props.menu.buildMenuBorderTopWithLabel('Inventory', 15), 'div_event_StoresPanel_buildMenuBorderTop'));
+        panelElements.push(this.props.menu.buildBreak('break_StoresPanel_buildMenuBorderTop'));
+
         if (this.props.model.stores == null) {
-            storesString = '| Your pockets are empty..';
+            panelElements.push(this.props.menu.buildMenuItem('Your pockets are empty..', 'div_StoresPanel_nothing_todo'));
+            panelElements.push(this.props.menu.buildBreak('break_StoresPanel_nothing_todo'));
         }
         else{
-            for (var key in this.props.model.stores){
-                var store_obj = this.props.model.stores[key]
-                var amount = store_obj['amount']
-                var description = store_obj['description']
-                storesString += "| " + key + " (" + amount + ")"
+            for (let key in this.props.model.stores){
+                let storesString = '';
+                let store_obj = this.props.model.stores[key];
+                let amount = store_obj['amount'];
+                let description = store_obj['description'];
+                storesString = key + " (" + amount + ")";
                 if (description != null) {
                     storesString += " (" + description + ")";
                 }
-                storesString += "\n";
+                panelElements.push(this.props.menu.buildMenuItem(storesString, 'div_StoresPanel_' + key));
+                panelElements.push(this.props.menu.buildBreak('break_StoresPanel_' + key));
             }
         }
-        return storesString;
+        panelElements.push(this.props.menu.buildLabel(this.props.menu.buildMenuBorderBottom(10), 'div_event_StoresPanel_buildMenuBorderBottom'));
+        panelElements.push(this.props.menu.buildBreak('break_StoresPanel_buildMenuBorderTop'));
+
+        return panelElements;
     }
 
     render() {
         return (
             <div className="StoresPanel">
-                <div>+-- stores ----------------</div>
-                <div>{this.buildStores()}</div>
-                <div>+--------------------------</div>
+                {this.buildStoresPanel()}
             </div>
         );
     }

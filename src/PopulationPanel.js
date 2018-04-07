79c7e66ae436470489cc28afc2d2e5ecc50a65b6
Joshua C. Burt <joshburt@shapeandshare.com>
2018-04-07 16:00:12 -0600

    converted to props provided menu lib

--- src/PopulationPanel.js
+++ src/PopulationPanel.js
@@ -2,12 +2,33 @@ import React, { Component } from 'react';
 import './PopulationPanel.css';
 
 class PopulationPanel extends Component {
+
+    buildPanel(){
+        let panelElements = [];
+
+        panelElements.push(this.props.menu.buildLabel(this.props.menu.buildMenuBorderTopWithLabel('population', 15), 'div_event_PopulationPanel_buildMenuBorderTop'));
+        panelElements.push(this.props.menu.buildBreak('break_PopulationPanel_buildMenuBorderTop'));
+
+        let popString = ''
+        if (this.props.model.population == undefined){
+            popString = '0'
+        }
+        else {
+            popString = this.props.model.population
+        }
+        panelElements.push(this.props.menu.buildMenuItem(popString, 'div_event_PopulationPanel_UserStatusGameState_population'));
+        panelElements.push(this.props.menu.buildBreak('break_PopulationPanel_population'));
+
+        panelElements.push(this.props.menu.buildLabel(this.props.menu.buildMenuBorderBottom(10), 'div_event_PopulationPanel_buildMenuBorderBottom'));
+        panelElements.push(this.props.menu.buildBreak('break_StatusPanel_buildMenuBorderTop'));
+
+        return panelElements;
+    }
+
     render() {
         return (
             <div className="PopulationPanel">
-                <div>+-- population --------</div>
-                <div>| {this.props.model.population}</div>
-                <div>+--------------------------</div>
+                {this.buildPanel()}
             </div>
         );
     }

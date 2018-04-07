356f6f1bf0ed29f3b12a9d052d5123df5f1691d2
Joshua C. Burt <joshburt@shapeandshare.com>
2018-04-06 21:16:44 -0600

    removed extra logging

--- src/Model.js
+++ src/Model.js
@@ -226,8 +226,7 @@ class Model extends React.Component {
                 if (responseJson.user.notifications.length > 0){
                     this.notifications.unshift(responseJson.user.notifications);
                 }
-
-                console.log(this.notifications);
+                
                 while (this.notifications.length > this.MAX_NOTIFICATIONS){
                     this.notifications.pop();
                 }

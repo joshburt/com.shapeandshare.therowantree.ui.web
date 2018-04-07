1af6f9eaf1223551ab27d0e3e1442b67bbafcf14
Joshua C. Burt <joshburt@shapeandshare.com>
2018-04-06 20:11:50 -0600

    unique ids

--- src/EventPanel.js
+++ src/EventPanel.js
@@ -13,6 +13,13 @@ class EventPanel extends Component {
         return(<br key={key_suffix}></br>);
     }
 
+    // https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
+    uuidv4() {
+        return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+            (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+        )
+    }
+    
     buildEventPanel() {
         let panelElements = [];
 
@@ -31,14 +38,14 @@ class EventPanel extends Component {
         panelElements.push(this.buildBreak(1));
 
         for (var index in this.props.model.notifications){
-            let note_obj = this.props.model.notifications[index];
-            if ('undefined' !== typeof note_obj) {
-                for (var note_item in note_obj) {
-                    let note_item_id = note_obj[note_item][0] + '_' + note_obj[note_item][1] + '_' + note_obj[note_item][2];
+            let event_collection = this.props.model.notifications[index];
+            if ('undefined' !== typeof event_collection) {
+                for (var event_obj in event_collection) {
+                    let note_item_id = this.uuidv4();
                     // panelElements.push(this.buildLabel('| DEBUG:' + note_obj[note_item][2], 'div_' + note_item_id));
                     // panelElements.push(this.buildBreak('break_' + note_item_id));
 
-                    let my_event = JSON.parse(note_obj[note_item][2]);
+                    let my_event = JSON.parse(event_collection[event_obj][2]);
                     let event_title = my_event['title'];
                     let event_text = my_event['text'];
                     let event_reward = my_event['reward'];
@@ -46,22 +53,22 @@ class EventPanel extends Component {
 
                     // build the event title
                     if (event_title !== undefined) {
-                        panelElements.push(this.buildLabel('| ' + event_title, 'div_' + note_item_id + event_title));
-                        panelElements.push(this.buildBreak('break_' + note_item_id + event_title));
+                        panelElements.push(this.buildLabel('| ' + event_title, 'div_event_title_' + note_item_id));
+                        panelElements.push(this.buildBreak('break_' + note_item_id));
                     }
                     if (event_text !== undefined) {
                         for (var line_index in event_text) {
-                            panelElements.push(this.buildLabel('| ' + event_text[line_index], 'div_event_text_' + line_index.toString()));
-                            panelElements.push(this.buildBreak('break_event_text' + line_index.toString()));
+                            panelElements.push(this.buildLabel('| ' + event_text[line_index], 'div_event_text_' + note_item_id + '_' + line_index.toString()));
+                            panelElements.push(this.buildBreak('break_event_text_' + note_item_id + '_' + line_index.toString()));
                         }
                     }
                     if (event_reward !== undefined){
-                        panelElements.push(this.buildLabel('| ' + JSON.stringify(event_reward), 'div_event_reward_' + JSON.stringify(event_reward) + '_' + note_item_id.toString()));
-                        panelElements.push(this.buildBreak('break_event_reward_' + JSON.stringify(event_reward)))
+                        panelElements.push(this.buildLabel('| ' + JSON.stringify(event_reward), 'div_event_reward_' + note_item_id));
+                        panelElements.push(this.buildBreak('break_event_reward_' + note_item_id))
                     }
                     if (event_boon !== undefined) {
-                        panelElements.push(this.buildLabel('| ' + JSON.stringify(event_boon), 'div_event_boon_' + JSON.stringify(event_boon) + '_' + note_item_id.toString()));
-                        panelElements.push(this.buildBreak('break_event_boon_' + JSON.stringify(event_boon)))
+                        panelElements.push(this.buildLabel('| ' + JSON.stringify(event_boon), 'div_event_boon_' + note_item_id));
+                        panelElements.push(this.buildBreak('break_event_boon_' + note_item_id))
                     }
 
                 }

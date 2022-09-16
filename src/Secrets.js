6540d780be4f06119e1e5b94119815deb4b50c25
Joshua Burt <joshburt@shapeandshare.com>
2022-09-16 10:32:53 -0700

    initial

--- src/Secrets.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-class Secrets extends React.Component {
-    static API_ACCESS_KEY = '3acb2d04-a15c-4a3b-a235-152a4ece876b';
-    static SERVER_BASE = 'http://localhost:8000';
-}
-
-export default Secrets;
